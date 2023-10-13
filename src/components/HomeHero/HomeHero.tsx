2af3feb8dc8a808cb556b097343b863819a75494
Abbos Nurgulshanov <abbos@teqqed.com>
2023-10-13 23:48:47 +0500

    mobile version updated and 3d shape added

--- src/components/HomeHero/HomeHero.tsx
+++ src/components/HomeHero/HomeHero.tsx
@@ -19,7 +19,7 @@ const HomeHero = () => {
 	return (
 		<Layout>
 			<div
-				className='-mt-[72px] w-full'
+				className='-mt-[72px] w-full overflow-hidden'
 				style={{
 					background:
 						'radial-gradient(113.04% 76.15% at 0% 0%, rgba(255, 255, 255, 0.2) 0%, rgba(255, 255, 255, 0) 100% ), #000',
@@ -27,21 +27,30 @@ const HomeHero = () => {
 			>
 				<div className='h-[72px] w-full block'></div>
 				<div className='flex items-center min-h-[calc(100vh-72px)] py-20 justify-center w-full px-5 h-full flex-col'>
-					<h1 className='text-center font-medium text-[48px] leading-[60px block] text-neutral-4 block mb-5'>
+					<h1 className='text-center font-medium text-[48px] leading-[60px block] text-neutral-4 block mb-5 xmd:text-[35px] xmd:leading-[35px] xmd:mb-4'>
 						Supercharge your business
 					</h1>
 					<p className='max-w-[325px] w-full mx-auto text-center text-neutral-10'>
 						Meet the platform that will help you 100X your service business.
 					</p>
-					<div className='max-w-[577px] w-full relative mx-auto p-2 gradient-border mt-[100px] before:rounded-2xl'>
+					<div className='max-w-[577px] w-full relative mx-auto p-2 gradient-border mt-[100px] before:rounded-2xl xmd:mt-[120px]'>
+						<div
+							className='absolute top-[-250px] left-[-450px] w-[651px] h-[651px] opacity-50 xmd:left-[-250px] xmd:top-[-180px] xmd:opacity-70'
+							style={{
+								background: 'url("./assets/images/3dshape.png")',
+								mixBlendMode: 'color-dodge',
+								flexShrink: 0,
+							}}
+						></div>
 						<div
 							className='absolute h-[1px] w-10 top-0 left-3 animated-line'
 							style={{
 								background:
-									'linear-gradient(90deg, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.3) )',
+									'linear-gradient(90deg, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.7) )',
 							}}
 						></div>
-						<div className='w-full p-2 before:rounded-xl gradient-border'>
+
+						<div className='w-full p-2 before:rounded-xl gradient-border relative z-[8]'>
 							<div className='w-full p-10 before:rounded-lg gradient-border-3 bg-[linear-gradient(180deg,rgba(255,255,255,0.08)0%,rgba(255,255,255,0.00)100%)] backdrop-blur-[45px]'>
 								<div className='flex justify-center mb-[6px]'>
 									<Image
@@ -55,18 +64,18 @@ const HomeHero = () => {
 									Join 2.000+ ambitious entrepreneurs.
 								</span>
 								<form onSubmit={form.handleSubmit}>
-									<div className='flex items-center w-full gap-3'>
+									<div className='flex items-center w-full gap-3 xmd:flex-col'>
 										<input
 											name='email'
 											value={form.values.email}
 											onChange={form.handleChange}
-											className='bg-transparent py-[11px] px-2 rounded-lg border border-[rgba(255,255,255,0.3)] text-base block flex-1 text-white  placeholder:text-neutral-10'
+											className='bg-transparent py-[11px] px-2 rounded-lg border border-[rgba(255,255,255,0.3)] text-base block flex-1 text-white  placeholder:text-neutral-10 xmd:w-full'
 											placeholder='Enter your email'
 										/>
 
 										<button
 											type='submit'
-											className='bg-neutral-4 rounded-lg text-lg h-[48px] px-6 transition-all duration-200 ease-linear hover:bg-white active:scale-90 border-none'
+											className='bg-neutral-4 rounded-lg text-lg h-[48px] px-6 transition-all duration-200 ease-linear hover:bg-white active:scale-90 border-none xmd:block xmd:w-full sm:text-center'
 										>
 											Join waitlist
 										</button>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,14 @@ const config: Config = {
 		'./src/**/*.{js,ts,jsx,tsx,mdx}',
 	],
 	theme: {
+		screens: {
+			xl: { max: '1440px' },
+			lg: { max: '1024px' },
+			md: { max: '768px' },
+			xmd: { max: '576px' },
+			sm: { max: '425px' },
+			xsm: { max: '375px' },
+		},
 		extend: {
 			colors: {
 				neutral: {
